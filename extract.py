25927207461c817a7440cf371899dd96b887bba4
IdhayaBastine15 <idhayabastine15@gmail.com>
2024-05-01 18:48:59 +0100

    Boiler plate done

--- extract.py
+++ extract.py
@@ -5,3 +5,31 @@ from dagster import op, Out, In, get_dagster_logger
 from datetime import date, datetime
 from pymongo import MongoClient, errors
 import numpy as np
+
+
+
+logger = get_dagster_logger()
+
+
+@op(
+    out=Out(bool)
+)
+
+def extract_unemployment() -> bool:
+    return []
+
+@op(
+    out=Out(bool)
+)
+
+def extract_quality_of_life() -> bool:
+    return []
+
+
+@op(
+    out=Out(bool)
+)
+
+def extract_cost_of_living() -> bool:
+    # Return the result flag
+    return []
--- transfer_and_load.py
+++ transfer_and_load.py
@@ -8,3 +8,59 @@ from sqlalchemy import create_engine, exc, VARCHAR, DECIMAL, INT, text
 from sqlalchemy.pool import NullPool
 from sqlalchemy.types import *
 import pandas.io.sql as sqlio
+
+
+logger = get_dagster_logger()
+
+@op(
+    ins={"start": In(bool)},
+    out=Out([])
+)
+
+def transform_cost_of_living(start):
+    return []
+
+@op(
+    ins={"start": In(bool)},
+    out=Out([])
+)
+
+def transform_quality_of_life(start):
+    return []
+
+
+
+@op(
+    ins={"start": In(bool)},
+    out=Out([])
+)
+
+def transform_unemployment(start):
+    return []
+
+
+@op(
+    ins={'dataframe1': [], 'dataframe2': [], 'dataframe3': []},
+    out=Out(pd.DataFrame)
+)
+
+def join(dataframe1, dataframe2, dataframe3) -> pd.DataFrame:
+    
+    return []
+
+    
+
+@op(
+    ins={'dataframe_merged': []},
+    out=Out(bool)
+)
+
+def load(merged_dataframe):
+    try:
+        # Return the number of rows inserted
+        return []
+    
+    # Trap and handle any relevant errors
+    except exc.SQLAlchemyError as error:
+        logger.error("Error: %s" % error)
+        return False
